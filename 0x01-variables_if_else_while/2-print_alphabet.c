ee61b2bfad55deac9d7370be72bfce0b81466abf
mohammed <hj72475@gmail.com>
2023-09-10 13:34:04 +0100

    solve task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function that print alphabet in lowcase
+ * Return: 0
+ */
+
+int main()
+{
+	int i;
+
+	for (i = 97; i = 122; i++)
+	{
+		putchar("%c", i);
+	}
+	return (0);
+}
