6f62d8facf26ad991b2df35e7e9ef128589431c4
mohammed <hj72475@gmail.com>
2023-09-13 14:34:11 +0100

    solve task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - function that print alphabet
+ * Return: 0
+ */
+
+int main(){
+	int i = 'a';
+
+	while(i = 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
