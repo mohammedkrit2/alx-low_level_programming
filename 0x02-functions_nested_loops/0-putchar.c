5ac312eb2873208b23df2de6004dd12725317783
mohammed <hj72475@gmail.com>
2023-09-13 13:43:45 +0100

    solve task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - function that print _putchar
+ * Return: 0
+ */
+
+int main(void){
+	_putchar(_putchar);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#define MAIN_H
+#ifndef MAIN_H
+int _putchar(char c);
+#endif
