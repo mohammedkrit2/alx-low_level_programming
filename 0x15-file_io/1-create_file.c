2881c323c57ba3b969071d068ce3d372ee1fde49
mohammed <hj72475@gmail.com>
2023-10-30 22:02:26 +0100

    1/3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strlen - returns the length of a string
+ * _strlen - returns the length of a string
  * @s: string whose length to check
  * Return: integer length of string
  */
